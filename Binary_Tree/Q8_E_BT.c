8f972ee4af0d0c0845db7b2c6170f8c44b4c10f3
kssumin <201566@jnu.ac.kr>
2024-04-14 14:51:40 +0000

    solve Q8_E_BT.c

--- Binary_Tree/Q8_E_BT.c
+++ Binary_Tree/Q8_E_BT.c
@@ -102,7 +102,25 @@ int main()
 
 int hasGreatGrandchild(BTNode *node)
 {
-	/* add your code here */
+	if (node == NULL)
+    {
+        return 0;
+    }
+    
+    int left = hasGreatGrandchild(node -> left);
+    int right = hasGreatGrandchild(node -> right);
+
+    if (left >=3 || right >=3)
+    {
+        printf("%d", node->item);
+    }
+
+    if (left < right)
+    {
+        return right + 1;
+    }
+
+    return left+1;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
