df4af8cd48009871937acf5424917bb5e61e8c40
kssumin <201566@jnu.ac.kr>
2024-04-16 11:04:22 +0000

    solve Q5_F_BST.c

--- Binary_Search_Tree/Q5_F_BST.c
+++ Binary_Search_Tree/Q5_F_BST.c
@@ -40,6 +40,8 @@ BSTNode *peek(Stack *s);
 int isEmpty(Stack *s);
 void removeAll(BSTNode **node);
 BSTNode* removeNodeFromTree(BSTNode *root, int value);
+BSTNode * findNode(BSTNode * root, int value);
+BSTNode * findMinNode(BSTNode * root);
 
 ///////////////////////////// main() /////////////////////////////////////////////
 
@@ -74,6 +76,17 @@ int main()
 			postOrderIterativeS2(root); // You need to code this function
 			printf("\n");
 			break;
+		case 3:
+			printf("before delete node ");
+			postOrderIterativeS2(root); 
+			printf("want remove node: ");
+			scanf("%d", &i);
+
+			removeNodeFromTree(root, i);
+			printf("after delete node");
+			postOrderIterativeS2(root); 
+			printf("\n");
+			break;
 		case 0:
 			removeAll(&root);
 			break;
@@ -89,17 +102,156 @@ int main()
 
 //////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * 후위 순회
+ *  1. 루트 노드를 젤 먼저 방문하게 되지만 젤 마지막으로 출력해야 한다.
+ *  2. 왼쪽 노드 -> 오른쪽 노드로 방문해야 한다.
+ * 
+ * 스택의 특성 상 먼저 들어간 노드가 젤 마지막에 나오게 된다.
+ * 따라서 루트 노드를 젤 먼저 넣고, 이후 오른쪽 왼쪽을 넣게 되면
+ * 스택에서 나오는 순서가 왼쪽 -> 오른쪽 -> 루트 노드이다.
+ * 
+*/
 void postOrderIterativeS2(BSTNode *root)
 {
-	 /* add your code here */
+	BSTNode * temp;
+	 if (root==NULL)
+	 {
+		return ;
+	 }
+
+	// 방문한 노드는 해당 스택에 저장한다
+	Stack * stack1 = malloc(sizeof(Stack));
+	stack1->top= NULL;
+
+	// 해당 스택을 pop하면 후위순회의 결과와 일치한다.
+	Stack * stack2 = malloc(sizeof(Stack));
+	stack2->top= NULL;
+
+	if (stack1 == NULL && stack2 == NULL)
+	{
+		return ;
+	}
+
+	push(stack1, root);
+
+	while(!isEmpty(stack1))
+	{
+		temp = pop(stack1);
+		push(stack2, temp);
+
+		if (temp -> left!=NULL)
+		{	
+			push(stack1, temp->left);
+		}
+
+		// 오른쪽을 stack1에 먼저 넣어야
+		// stack1에서 왼쪽보다 더 먼저 나온다.
+		// 따라서 stack2에는 더 먼저 들어가기 때문에
+		// 오른쪽이 왼쪽보다 더 늦게 출력된다.
+		if (temp ->right!=NULL)
+		{	
+			push(stack1, temp->right);
+		}
+	}
+	
+
+	while (!isEmpty(stack2))
+	{
+		printf("%d ", pop(stack2)->item);
+	}
 }
 
 /* Given a binary search tree and a key, this function
    deletes the key and returns the new root. Make recursive function. */
 BSTNode* removeNodeFromTree(BSTNode *root, int value)
-{
-	/* add your code here */
+{	
+	BSTNode * temp;
+
+	if (root == NULL)
+	{
+		return NULL;
+	}
+
+	if (root->item < value)
+	{
+		root ->right = removeNodeFromTree(root -> right, value);
+	}
+	if (root -> item >value)
+	{
+		root -> left = removeNodeFromTree(root -> left, value);
+	}
+	if (root -> item == value)
+	{
+		// 삭제하고자 하는 노드가 리프노드라면
+		if (root->left ==NULL && root->right == NULL)
+		{
+			free(root);
+			printf("%s\n", "왜 종료안해?");
+			return NULL;
+		}
+
+		// 오른쪽 서브노드만 존재한다면
+		if (root ->left ==NULL && root -> right !=NULL)
+		{
+			free(root);
+			return root->right;
+		}
+
+		// 왼쪽 서브노드만 존재한다면
+		if (root ->left !=NULL && root -> right ==NULL)
+		{
+			free(root);
+			return root->left;
+		}
+
+		// 삭제하고자 하는 해당 루트노드가
+		// 왼쪽 오른쪽 모두 서브트리가 존재한다면
+		// 작은 값 중에서 가장 큰 값을 찾는다
+		temp = findMinNode(root->left);
+
+		root->item = temp->item;
+
+		root->left = removeNodeFromTree(root->left, temp->item);
+	}
+	
+	return root;
 }
+
+BSTNode * findNode(BSTNode * root, int value)
+{	
+	if (root == NULL)
+	{
+		return NULL;
+	}
+
+	if (root->item == value)
+	{
+		return root;
+	}
+	
+	// 찾고자 하는 값이 루트 노드의 값보다 더 크다면 
+	// 오른쪽 서브 트리를 탐색한다
+	if (root->item < value)
+	{
+		return findNode(root->right, value);
+	}
+
+	// 찾고자 하는 값이 루트 노드의 값보다 더 작다면
+	// 왼쪽 서브 트리를 탐색한다
+	return findNode(root ->left, value);
+}
+
+BSTNode * findMinNode(BSTNode * root)
+{	
+	if (root->right == NULL)
+	{
+		return root;
+	}
+
+	return findMinNode(root->right);
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 
 void insertBSTNode(BSTNode **node, int value){
